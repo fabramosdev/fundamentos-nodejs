476e2b73348fcb15ecf66ef82bfa7d775db804a9
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2023-03-18 12:19:06 -0300

    add http methods

--- src/index.js
+++ src/index.js
@@ -6,6 +6,27 @@ const port = 3333
 app.get('/', (request, response) => {
     return response.json({message: `Alô mundo na porta ${port}!`})
 })
+
+app.get('/courses', (request, response) => {
+    return response.json(["Java", "Node", "React"])
+})
+
+app.post("/courses", (request, response) => {
+    return response.json(["Java", "Node", "React", "React Native"])
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Javascript", "Node", "React", "React Native"])
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Javascript", "NodeJS", "React", "React Native"])
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Javascript", "NodeJS", "React"])
+})
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
