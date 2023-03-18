7e1f6b01e2ee2512b7443382922ebb82e1c07a82
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2023-03-18 20:31:53 -0300

    add express json

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 const express = require('express')
-
 const app = express()
 const port = 3333
 
+app.use(express.json())
 app.get('/', (request, response) => {
     return response.json({message: `Alô mundo na porta ${port}!`})
 })
 
 app.get('/courses', (request, response) => {
+    const query = request.query
+    console.log(query)
     return response.json(["Java", "Node", "React"])
 })
 
 app.post("/courses", (request, response) => {
+    const body = request.body
+    console.log(body)
     return response.json(["Java", "Node", "React", "React Native"])
 })
 
 app.put("/courses/:id", (request, response) => {
+    const params = request.params
+    const { id } = request.params
+    console.log(params, id)
     return response.json(["Javascript", "Node", "React", "React Native"])
 })
 
---
+++ src/metodos_e_parametros.md
@@ -0,0 +1,17 @@
+# Metodos e Parametros
+
+## Metodos
+
+### GET => Buscar informação no server
+### POST => Inserir informação no server
+### PUT => Alterar informação no server
+### PATCH => Alterar informação _ESPECÍFICA_ no server
+### DELETE => Apagar informação no server
+
+==========================================================
+
+## Parâmetros
+
+### Route Params => Identificar um recurso para editar/deletar/buscar (Parametros que vem dentro da rota)
+### Query Params => Paginação/Filtro
+### Body Params => Objetos para inserção/alteração
